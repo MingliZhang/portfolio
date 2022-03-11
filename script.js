08df57257f7d2c5535d8b986bc2db45cb27a499b
MingliZhang <31600683+MingliZhang@users.noreply.github.com>
2022-03-10 22:15:07 -0500

    tried but wont work

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import {
+	getFirestore,
+	doc,
+	setDoc,
+	collection,
+	onSnapshot,
+	getDocs,
+	deleteDoc,
+	updateDoc,
+} from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyB05mlBu0_zFGq2WVlCZJT11FX0g-3uczk',
+	authDomain: 'portfolio-a6fab.firebaseapp.com',
+	databaseURL: 'https://portfolio-a6fab-default-rtdb.firebaseio.com',
+	projectId: 'portfolio-a6fab',
+	storageBucket: 'portfolio-a6fab.appspot.com',
+	messagingSenderId: '230907708278',
+	appId: '1:230907708278:web:6dc81647eb0a48e9f1b057',
+	measurementId: 'G-RTRJEJQ5P4',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore();
+
 const nameElement = document.getElementById('name');
 const subjectElement = document.getElementById('subject');
 const emailElement = document.getElementById('email');
@@ -18,32 +55,25 @@ const handleSendButton = async function (event) {
 	event.preventDefault();
 	event.stopPropagation();
 
-	const name = nameElement.value;
-	const subject = subjectElement.value;
-	const email = emailElement.value;
-	const message = messageElement.value;
+	let data = {
+		name: nameElement.value,
+		subject: subjectElement.value,
+		email: emailElement.value,
+		message: messageElement.value,
+	};
 
 	if (
-		name.length === 0 ||
-		subject.length === 0 ||
-		email.length === 0 ||
-		message.length === 0
+		data.name.length === 0 ||
+		data.subject.length === 0 ||
+		data.email.length === 0 ||
+		data.message.length === 0
 	) {
 		respond.innerHTML = `<p>Please enter all of the fields above!</p>`;
 	} else {
 		if (ValidateEmail(email)) {
 			respond.innerHTML = `<p>Processing, please wait.</p>`;
 			try {
-				const result = await axios({
-					method: 'post',
-					url: 'https://us-central1-portfolio-a6fab.cloudfunctions.net/messages',
-					data: {
-						name: name,
-						email: email,
-						message: message,
-						subject: subject,
-					},
-				});
+				await setDoc(doc(db, 'messages', data.message), data);
 				respond.innerHTML = `<p>The message has been recieved.</p>`;
 				nameElement.value = '';
 				subjectElement.value = '';
