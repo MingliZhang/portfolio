c28cb9702896126d5c1f3626cf4a0194897d71b7
MingliZhang <31600683+MingliZhang@users.noreply.github.com>
2022-03-10 22:42:20 -0500

    tried to fix it...

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,21 +17,6 @@ import {
 	deleteDoc,
 	updateDoc,
 } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js';
-
-const firebaseConfig = {
-	apiKey: 'AIzaSyB05mlBu0_zFGq2WVlCZJT11FX0g-3uczk',
-	authDomain: 'portfolio-a6fab.firebaseapp.com',
-	databaseURL: 'https://portfolio-a6fab-default-rtdb.firebaseio.com',
-	projectId: 'portfolio-a6fab',
-	storageBucket: 'portfolio-a6fab.appspot.com',
-	messagingSenderId: '230907708278',
-	appId: '1:230907708278:web:6dc81647eb0a48e9f1b057',
-	measurementId: 'G-RTRJEJQ5P4',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const db = getFirestore();
 
